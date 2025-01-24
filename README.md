abc640f5569f09deff7d5aa564a6ba54aef4f200
Aitik Dandapat <aitikdandapat@Aitiks-MacBook-Air.local>
2025-01-24 18:44:56 -0500

    Added Text Box

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Story-Generator-from-Images
+
+## A Large Language Model Based App to Generate Stories from Pictures
+
+<a href="https://huggingface.co/spaces/aitik2000/Story-Generator-from-Images"  target="_blank"><img src="https://img.shields.io/badge/click_here_to_open_gradio_app-orange?style=for-the-badge&logo=dependabot"/></a>
+
+<img src="robot-sits-grass-reading-book_524325-338.jpg" width="1000" height="350"/><br><br> 
--- app.py
+++ app.py
@@ -8,23 +8,24 @@ from transformers import pipeline
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model_path = ("../Models/models--Salesforce--blip-image-captioning-large"
-              "/snapshots/2227ac38c9f16105cb0412e7cab4759978a8fd90")
+# model_path = ("../Models/models--Salesforce--blip-image-captioning-large"
+#               "/snapshots/2227ac38c9f16105cb0412e7cab4759978a8fd90")
 
-tts_model_path = ("../Models/models--kakao-enterprise--vits-ljs/snapshots"
-                  "/3bcb8321394f671bd948ebf0d086d694dda95464")
+# tts_model_path = ("../Models/models--kakao-enterprise--vits-ljs/snapshots"
+#                   "/3bcb8321394f671bd948ebf0d086d694dda95464")
 
 # caption_image = pipeline("image-to-text",
-#                 model="Salesforce/blip-image-captioning-large", device=device)
-#
+#                          model=model_path, device=device)
+
 # narrator = pipeline("text-to-speech",
-#                     model="kakao-enterprise/vits-ljs")
+#                     model=tts_model_path)
 
 caption_image = pipeline("image-to-text",
-                model=model_path, device=device)
+                model="Salesforce/blip-image-captioning-large", device=device)
 
 narrator = pipeline("text-to-speech",
-                    model=tts_model_path)
+                    model="kakao-enterprise/vits-ljs")
+
 
 def generate_audio(text):
     # Generate the narrated text
@@ -38,12 +39,24 @@ def generate_audio(text):
 
 
 def caption_my_image(pil_image):
+    # Generate the caption
     semantics = caption_image(images=pil_image)[0]['generated_text']
-    return generate_audio(semantics)
+    # Generate the audio for the caption
+    audio_path = generate_audio(semantics)
+    # Return both the caption and the audio
+    return semantics, audio_path
 
-demo = gr.Interface(fn=caption_my_image,
-                    inputs=[gr.Image(label="Select Image",type="pil")],
-                    outputs=[gr.Audio(label="Image Caption")],
-                    title="@GenAILearniverse Project 8: Image Captioning",
-                    description="THIS APPLICATION WILL BE USED TO CAPTION THE IMAGE.")
+
+# Define the Gradio interface
+demo = gr.Interface(
+    fn=caption_my_image,
+    inputs=[gr.Image(label="Select Image", type="pil")],
+    outputs=[
+        gr.Textbox(label="Generated Caption"),
+        gr.Audio(label="Image Caption")
+    ],
+    title="Story Generation From Images",
+    description="THIS APPLICATION WILL BE USED TO GENERATE STORY OF THE IMAGE."
+)
+
 demo.launch()
